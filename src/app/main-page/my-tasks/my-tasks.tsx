8e74b0dcb885980ef02dd3ddc09def05fe3dcc1f
Błażej Mzyk <blazej.mzyk@gmail.com>
2023-12-14 18:17:48 +0100

    Added mock tasks

--- src/app/main-page/my-tasks/my-tasks.tsx
+++ src/app/main-page/my-tasks/my-tasks.tsx
@@ -1,1 +1,57 @@
+"use client";
+import { Task, TaskPriority, TaskSize, TaskStatus } from "@/shared/task.model";
 import "./my-tasks.css";
+
+export function MyTasks() {
+  const mockTasks: Task[] = [
+    {
+      status: TaskStatus.TODO,
+      name: "Task 1",
+      priority: TaskPriority.HIGH,
+      size: TaskSize.L,
+      details: "Task 1 details",
+    },
+    {
+      status: TaskStatus.IN_PROGRESS,
+      name: "Task 2",
+      priority: TaskPriority.MEDIUM,
+      size: TaskSize.M,
+      details: "Task 2 details",
+    },
+    {
+      status: TaskStatus.DONE,
+      name: "Task 3",
+      priority: TaskPriority.HIGH,
+      size: TaskSize.S,
+      details: "Task 3 details",
+    },
+  ];
+
+  return (
+    <div>
+      My tasks
+      <table>
+        <thead>
+          <tr>
+            <th>Status</th>
+            <th>Task</th>
+            <th>Priority</th>
+            <th>Size</th>
+            <th>Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mockTasks.map((task, index) => (
+            <tr key={`${task.name} ${index}`}>
+              <td>{task.status}</td>
+              <td>{task.name}</td>
+              <td>{task.priority}</td>
+              <td>{task.size}</td>
+              <td>{task.details}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- src/app/main-page/page.tsx
+++ src/app/main-page/page.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import "./main-page.css";
 import { Sidebar } from "../shared/sidebar/sidebar";
+import { MyTasks } from "./my-tasks/my-tasks";
 
 export default function MainPage() {
   return (
     <section id="MainPage" className="main-page-container">
       <Sidebar />
-      MainPage
+      <MyTasks />
     </section>
   );
 }
